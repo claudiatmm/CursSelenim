89efe7db6f0f442fab3570181a42494a9142892e
Claudia Timiș <claudia.timis95@gmail.com>
2022-03-26 14:02:10 +0200

    commit in master

---
+++ src/test/java/TEMAcurs9GIT/temaFile1.java
@@ -0,0 +1,14 @@
+package TEMAcurs9GIT;
+
+import org.testng.annotations.Test;
+
+public class temaFile1 {
+
+    /// test git fisier 2
+
+    public static void main(String [] arg){
+        System.out.println("testFisier1");
+    }
+
+
+}
---
+++ src/test/java/TEMAcurs9GIT/temaFile2.java
@@ -0,0 +1,12 @@
+package TEMAcurs9GIT;
+
+public class temaFile2 {
+
+
+    //test fisier pentru GIT
+
+    public static void main(String [] arg){
+        System.out.println("testFisier1");
+    }
+
+}
